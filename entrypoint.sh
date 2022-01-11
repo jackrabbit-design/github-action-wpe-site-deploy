3896211ebeacc232338de2aab20f611bc5c4a7a7
Alvin Novian <alvinnovian883@gmail.com>
2022-01-11 16:03:19 +0700

    chore: try git push

--- entrypoint.sh
+++ entrypoint.sh
@@ -90,7 +90,7 @@ if [ "${INPUT_WITH_GIT_PUSH^^}" == "TRUE" ]; then
     echo "Begin Git push into $WPE_GIT_DESTINATION"
     echo "With env    : $WPE_ENV_NAME"
     echo "From branch : $GITHUB_REF"
-    git push $WPE_ENV_NAME $GITHUB_REF:master --force
+    git push $WPE_ENV_NAME $GITHUB_REF:master --force --verbose
     echo "Git push Successful! No errors detected!"
 else 
     echo "Skipping Git push."
